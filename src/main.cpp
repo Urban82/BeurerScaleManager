fc638f59903414ada83eef8432c46870f4a89948
Danilo Treffiletti <urban82@gmail.com>
2014-06-30 15:08:52 +0200

    Added file header for documentation and copyright

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,31 @@
+/*!
+ * \file main.cpp
+ * \author Danilo Treffiletti <urban82@gmail.com>
+ * \date 2014-06-29
+ * \copyright 2014 (c) Danilo Treffiletti
+ *
+ * \brief Starting point for the application.
+ *
+ * This project would be a Linux replacement for the Beurer HealthManager (see
+ * https://connect.beurer.com/Download/Common/Software.aspx) that is available
+ * only for Windows(r).
+ *
+ *    This file is part of BeurerScaleManager.
+ *
+ *    BeurerScaleManager is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    BeurerScaleManager is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with BeurerScaleManager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <QDebug>
 
 int main(int argc, char** argv)
