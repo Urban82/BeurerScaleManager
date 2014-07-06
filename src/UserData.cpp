a05332e59ef857c88ac07f0f5b2c52c9c335149e
Danilo Treffiletti <urban82@gmail.com>
2014-07-06 14:41:15 +0200

    Added UserData class

---
+++ src/UserData.cpp
@@ -0,0 +1,106 @@
+/*!
+ * \file UserData.cpp
+ * \author Danilo Treffiletti <urban82@gmail.com>
+ * \date 2014-07-05
+ * \brief Implementation for the UserData class
+ * \copyright 2014 (c) Danilo Treffiletti
+ *
+ *    This file is part of BeurerScaleManager.
+ *
+ *    BeurerScaleManager is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    BeurerScaleManager is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with BeurerScaleManager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "UserData.hpp"
+
+UserData::UserData(QObject* parent)
+    : QObject(parent)
+    , m_height(0)
+    , m_gender(Unknown)
+    , m_activity(None)
+{
+}
+
+UserData::UserData(const UserData& other, QObject* parent)
+    : QObject(parent)
+    , m_birthDate(other.m_birthDate)
+    , m_height(other.m_height)
+    , m_gender(other.m_gender)
+    , m_activity(other.m_activity)
+    , m_measurements(other.m_measurements)
+{
+}
+
+UserData::~UserData()
+{
+}
+
+UserData& UserData::operator=(const UserData& other)
+{
+    m_birthDate = other.m_birthDate;
+    m_height = other.m_height;
+    m_gender = other.m_gender;
+    m_activity = other.m_activity;
+    m_measurements = other.m_measurements;
+    return *this;
+}
+
+QDate UserData::getBirthDate() const
+{
+    return m_birthDate;
+}
+
+void UserData::setBirthDate(const QDate& birthDate)
+{
+    m_birthDate = birthDate;
+}
+
+ushort UserData::getHeight() const
+{
+    return m_height;
+}
+
+void UserData::setHeight(const ushort& height)
+{
+    m_height = height;
+}
+
+UserData::Gender UserData::getGender() const
+{
+    return m_gender;
+}
+
+void UserData::setGender(const UserData::Gender& gender)
+{
+    m_gender = gender;
+}
+
+UserData::Activity UserData::getActivity() const
+{
+    return m_activity;
+}
+
+void UserData::setActivity(const UserData::Activity& activity)
+{
+    m_activity = activity;
+}
+
+UserMeasurementList& UserData::getMeasurements()
+{
+    return m_measurements;
+}
+
+void UserData::setMeasurements(const UserMeasurementList& measurements)
+{
+    m_measurements = measurements;
+}
---
+++ src/UserData.hpp
@@ -0,0 +1,164 @@
+/*!
+ * \file UserData.hpp
+ * \author Danilo Treffiletti <urban82@gmail.com>
+ * \date 2014-07-05
+ * \brief Header for the UserData class
+ * \copyright 2014 (c) Danilo Treffiletti
+ *
+ *    This file is part of BeurerScaleManager.
+ *
+ *    BeurerScaleManager is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    BeurerScaleManager is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with BeurerScaleManager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef USERDATA_HPP
+#define USERDATA_HPP
+
+#include <QtCore/QObject>
+#include <QtCore/QDate>
+
+#include "UserMeasurement.hpp"
+
+/*!
+ * \class UserData
+ * \brief Measurements and personal data for the user.
+ *
+ * This class holds the measurements data and the personal data for each user.
+ *
+ * The measurements data are presented as a list of UserMeasurement, the personal
+ * data are presented as single value: birth date, height, gender and degree of
+ * activity.
+ */
+class UserData : public QObject
+{
+    Q_OBJECT
+
+    /*! Gender enumerator.
+     * \sa gender
+     */
+    enum Gender {
+        Unknown,    //!< Unknown gender
+        Male,       //!< Male
+        Female      //!< Female
+    };
+
+    /*! Degree of activity.
+     *
+     * The values are reported from the Beurer user manual of the scale BF 480 USB.
+     * \sa activity
+     */
+    enum Activity {
+        None,       //!< No activity
+        Low,        //!< A small amount of light physical effort (e.g. short walk, light garden work, gymnastic exercises)
+        Medium,     //!< Physical effort for 30 minutes at least 2 to 4 times a week
+        High,       //!< Physical effort for 30 minutes at least 4 to 6 times a week
+        VeryHigh,   //!< Intensive physical effort, intensive training or hard physical work for at least one hour daily
+    };
+
+    /*! The birth date of the user.
+     * \sa getBirthDate setBirthDate
+     */
+    Q_PROPERTY(QDate birthDate READ getBirthDate WRITE setBirthDate);
+    /*! The height of the user (in cm).
+     * \sa getHeight setHeight
+     */
+    Q_PROPERTY(ushort height READ getHeight WRITE setHeight);
+    /*! The gender of the user.
+     * \sa Gender getGender setGender
+     */
+    Q_PROPERTY(Gender gender READ getGender WRITE setGender);
+    /*! The degree of activity of the user.
+     * \sa Activity getActivity setActivity
+     */
+    Q_PROPERTY(Activity activity READ getActivity WRITE setActivity);
+    /*! The measurements of the user.
+     * \sa UserMeasurement UserMeasurementList getMeasurements setMeasurements
+     */
+    Q_PROPERTY(UserMeasurementList measurements READ getMeasurements WRITE setMeasurements);
+
+public:
+    /*! Constructor of the class.
+     * \param parent the parent QObject
+     */
+    explicit UserData(QObject* parent = 0);
+    /*! Copy constructor of the class.
+     * \param other the other UserData to copy
+     * \param parent the parent QObject
+     */
+    UserData(const UserData& other, QObject* parent = 0);
+    virtual ~UserData();
+
+    /*! Assignment operator.
+     * \param other the other UserData to copy
+     */
+    virtual UserData& operator=(const UserData& other);
+
+    /*! Getter for the birthDate property.
+     * \sa birthDate setBirthDate
+     */
+    QDate getBirthDate() const;
+    /*! Setter for the birthDate property.
+     * \param birthDate the new value
+     * \sa birthDate getBirthDate
+     */
+    void setBirthDate(const QDate& birthDate);
+
+    /*! Getter for the height property.
+     * \sa height setHeight
+     */
+    ushort getHeight() const;
+    /*! Setter for the height property.
+     * \param height the new value
+     * \sa height getHeight
+     */
+    void setHeight(const ushort& height);
+
+    /*! Getter for the gender property.
+     * \sa Gender gender setGender
+     */
+    Gender getGender() const;
+    /*! Setter for the gender property.
+     * \param gender the new value
+     * \sa Gender gender getGender
+     */
+    void setGender(const Gender& gender);
+
+    /*! Getter for the activity property.
+     * \sa Activity activity setActivity
+     */
+    Activity getActivity() const;
+    /*! Setter for the activity property.
+     * \param activity the new value
+     * \sa Activity activity getActivity
+     */
+    void setActivity(const Activity& activity);
+
+    /*! Getter for the measurements property.
+     * \sa UserMeasurement UserMeasurementList measurements setMeasurements
+     */
+    UserMeasurementList& getMeasurements();
+    /*! Setter for the measurements property.
+     * \param measurements the new value
+     * \sa UserMeasurement UserMeasurementList measurements getMeasurements
+     */
+    void setMeasurements(const UserMeasurementList& measurements);
+
+protected:
+    QDate m_birthDate;                  //!< birthDate property value.      \sa birthDate getBirthDate setBirthDate
+    ushort m_height;                    //!< height property value.         \sa height getHeight setHeight
+    Gender m_gender;                    //!< gender property value.         \sa gender getGender setGender
+    Activity m_activity;                //!< activity property value.       \sa activity getActivity setActivity
+    UserMeasurementList m_measurements; //!< measurements property values.  \sa measurements getMeasurements setMeasurements
+};
+
+#endif // USERDATA_HPP
