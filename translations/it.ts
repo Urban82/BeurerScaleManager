e4b377e3f77d96fd57c7fc2a3cd69174384514a4
Danilo Treffiletti <urban82@gmail.com>
2015-01-25 16:12:47 +0100

    Updated translation file

--- translations/it.ts
+++ translations/it.ts
@@ -4,54 +4,54 @@
 <context>
     <name>BSM::BeurerScaleManager</name>
     <message>
-        <location filename="../src/BeurerScaleManager.cpp" line="100"/>
-        <location filename="../src/BeurerScaleManager.cpp" line="109"/>
-        <location filename="../src/BeurerScaleManager.cpp" line="116"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="98"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="108"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="115"/>
         <source>New scale user</source>
         <translation>Nuovo utente della bilancia</translation>
     </message>
     <message>
-        <location filename="../src/BeurerScaleManager.cpp" line="101"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="99"/>
         <source>It seems that a new user was added on the scale.</source>
         <translation>Sembra che un nuovo utente sia stato aggiunto nella bilancia.</translation>
     </message>
     <message>
-        <location filename="../src/BeurerScaleManager.cpp" line="103"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="101"/>
         <source>Do you want to add an account for the user with ID %1?</source>
         <translation>Si vuole creare un account per l&apos;utente con ID %1?</translation>
     </message>
     <message>
-        <location filename="../src/BeurerScaleManager.cpp" line="110"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="109"/>
         <source>Insert a name for the new user:</source>
         <translation>Inserire un nome per il nuovo utente:</translation>
     </message>
     <message>
-        <location filename="../src/BeurerScaleManager.cpp" line="117"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="116"/>
         <source>Please insert a valid name for the user!</source>
         <translation>Si prega di inserire un nuome valido per l&apos;utente!</translation>
     </message>
     <message>
-        <location filename="../src/BeurerScaleManager.cpp" line="138"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="165"/>
         <source>Wrong scale settings</source>
         <translation>Impostazioni bilancia errati</translation>
     </message>
     <message>
-        <location filename="../src/BeurerScaleManager.cpp" line="139"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="166"/>
         <source>The date and time set in the scale (%1) are not correct!</source>
         <translation>La data e l&amp;apos;orario impostati nella bilancia (%1) non sono corretti!</translation>
     </message>
     <message>
-        <location filename="../src/BeurerScaleManager.cpp" line="141"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="168"/>
         <source>Please check the settings.</source>
         <translation>Si prega di controllare le impostazioni.</translation>
     </message>
     <message>
-        <location filename="../src/BeurerScaleManager.cpp" line="154"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="181"/>
         <source>No scale found or download error!&lt;br&gt;&lt;br&gt;Please check USB cable and try again.</source>
         <translation>Nessuna bilancia trovata o errore nel download!&lt;br&gt;&lt;br&gt;Si prega di controllare il cavo USB e riprovare nuovamente.</translation>
     </message>
     <message>
-        <location filename="../src/BeurerScaleManager.cpp" line="153"/>
+        <location filename="../src/BeurerScaleManager.cpp" line="180"/>
         <source>Download error</source>
         <translation>Errore download</translation>
     </message>
@@ -92,12 +92,12 @@
 <context>
     <name>BSM::Data::UserData</name>
     <message>
-        <location filename="../src/Data/UserData.cpp" line="87"/>
+        <location filename="../src/Data/UserData.cpp" line="86"/>
         <source>M</source>
         <translation>M</translation>
     </message>
     <message>
-        <location filename="../src/Data/UserData.cpp" line="89"/>
+        <location filename="../src/Data/UserData.cpp" line="88"/>
         <source>F</source>
         <translation>F</translation>
     </message>
@@ -105,58 +105,58 @@
 <context>
     <name>BSM::Utils</name>
     <message>
-        <location filename="../src/utils.cpp" line="88"/>
+        <location filename="../src/utils.cpp" line="90"/>
         <source>Directory not found</source>
         <translation>Cartella non trovata</translation>
     </message>
     <message>
-        <location filename="../src/utils.cpp" line="89"/>
+        <location filename="../src/utils.cpp" line="91"/>
         <source>Cannot find user directory &quot;%1&quot;.&lt;br&gt;&lt;br&gt;Please check your environment.</source>
         <translation>Impossibile trovare la cartella dell&apos;utente &quot;%1&quot;.&lt;br&gt;&lt;br&gt;Si prega di controllare l&apos;ambiente.</translation>
     </message>
     <message>
-        <location filename="../src/utils.cpp" line="100"/>
+        <location filename="../src/utils.cpp" line="102"/>
         <source>Directory not created</source>
         <translation>Cartella non creata</translation>
     </message>
     <message>
-        <location filename="../src/utils.cpp" line="101"/>
+        <location filename="../src/utils.cpp" line="103"/>
         <source>Cannot create user saving directory &quot;%1&quot;.&lt;br&gt;&lt;br&gt;Please check your environment.</source>
         <translation>Impossibile creare la cartella per i dati &quot;%1&quot;.&lt;br&gt;&lt;br&gt;Si prega di controllare l&apos;ambiente.</translation>
     </message>
     <message>
-        <location filename="../src/utils.cpp" line="109"/>
+        <location filename="../src/utils.cpp" line="111"/>
         <source>Directory not opened</source>
         <translation>Cartella non aperta</translation>
     </message>
     <message>
-        <location filename="../src/utils.cpp" line="110"/>
+        <location filename="../src/utils.cpp" line="112"/>
         <source>Cannot open user saving directory &quot;%1&quot;.&lt;br&gt;&lt;br&gt;Please check your environment.</source>
         <translation>Impossibile aprire la cartella per i dati &quot;%1&quot;.&lt;br&gt;&lt;br&gt;Si prega di controllare l&apos;ambiente.</translation>
     </message>
     <message>
-        <location filename="../src/utils.cpp" line="136"/>
+        <location filename="../src/utils.cpp" line="138"/>
         <source>Database not opened</source>
         <translation>Batabase non aperto</translation>
     </message>
     <message>
-        <location filename="../src/utils.cpp" line="137"/>
+        <location filename="../src/utils.cpp" line="139"/>
         <source>Cannot open the database &quot;%1&quot;.&lt;br&gt;&lt;br&gt;Please check your environment.</source>
         <translation>Impossibile aprire il database &quot;%1&quot;.&lt;br&gt;&lt;br&gt;Si prega di controllare l&apos;ambiente.</translation>
     </message>
     <message>
-        <location filename="../src/utils.cpp" line="146"/>
-        <location filename="../src/utils.cpp" line="161"/>
+        <location filename="../src/utils.cpp" line="148"/>
+        <location filename="../src/utils.cpp" line="167"/>
         <source>Cannot create table</source>
         <translation>Impossibile creare la tabella</translation>
     </message>
     <message>
-        <location filename="../src/utils.cpp" line="147"/>
+        <location filename="../src/utils.cpp" line="149"/>
         <source>Cannot create table &quot;%1&quot;.&lt;br&gt;&lt;br&gt;Please check your environment.</source>
         <translation>Impossibile creare la tabella &quot;%1&quot;.&lt;br&gt;&lt;br&gt;Si prega di controllare l&apos;ambiente.</translation>
     </message>
     <message>
-        <location filename="../src/utils.cpp" line="162"/>
+        <location filename="../src/utils.cpp" line="168"/>
         <source>Cannot create the following tables: %1.&lt;br&gt;&lt;br&gt;Please check your environment.</source>
         <translation>Impossibile creare le seguenti tabelle: %1.&lt;br&gt;&lt;br&gt;Si prega di controllare l&apos;ambiente.</translation>
     </message>
